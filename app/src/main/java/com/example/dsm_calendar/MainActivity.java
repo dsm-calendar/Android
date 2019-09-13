2342a319931bb9c91eebe17a791e36db96183cf4
seung min <3235tmdals@gmail.com>
2019-09-13 18:06:27 +0900

    saving commit as my laptop has low battery. now has runtime error with drawer gravity

--- app/src/main/java/com/example/dsm_calendar/MainActivity.java
+++ app/src/main/java/com/example/dsm_calendar/MainActivity.java
@@ -1,17 +1,29 @@
 package com.example.dsm_calendar;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toolbar;
+import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager());
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +34,68 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(adapter);
         viewPager.setCurrentItem(1);
 
+        toolbar = findViewById(R.id.main_toolbar);
+
         TabLayout tabLayout = findViewById(R.id.main_tabBar);
         tabLayout.setupWithViewPager(viewPager);
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+
+        setToolBar();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_toolbar_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                break;
+            case R.id.mail:
+                Toast.makeText(this, "mail box", Toast.LENGTH_SHORT).show();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.setting:
+                Toast.makeText(this, "setting", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.code:
+                Toast.makeText(this, "code", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.exit:
+                Toast.makeText(this, "logout", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        drawerLayout.closeDrawers();
+        return false;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawers();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    public void setToolBar(){
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_list);
     }
 }
