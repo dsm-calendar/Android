3f0056d927450f8f386dc6b029b997277b942333
seung min <3235tmdals@gmail.com>
2019-09-16 20:10:52 +0900

    now can see images but not indicators

--- app/src/main/java/com/example/dsm_calendar/MainFragment.java
+++ app/src/main/java/com/example/dsm_calendar/MainFragment.java
@@ -1,22 +1,21 @@
 package com.example.dsm_calendar;
 
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Adapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
-
-import com.google.android.material.tabs.TabLayout;
 
 import java.util.ArrayList;
 
@@ -56,12 +55,44 @@ public class MainFragment extends Fragment implements RadioButton.OnClickListene
         rootView.findViewById(R.id.notice_radioButton).setOnClickListener(this);
         rootView.findViewById(R.id.schedule_radioButton).setOnClickListener(this);
 
-        ArrayList<Drawable> obj = new ArrayList<>();
+        Integer[] obj = {R.drawable.sample_car2, R.drawable.sample_rainbow};
+
         MainVPBannerAdapter adapter = new MainVPBannerAdapter(getContext(), obj);
         ViewPager pager = rootView.findViewById(R.id.main_viewPager);
         pager.setAdapter(adapter);
-        TabLayout tabLayout = rootView.findViewById(R.id.main_vp_tab);
-        tabLayout.setupWithViewPager(pager, true);
+        LinearLayout sliderDotsPanel = rootView.findViewById(R.id.slider_dots);
+        int dotsCount = adapter.getCount();
+        ImageView[] dots = new ImageView[dotsCount];
+
+
+        for (int i = 0; i < dotsCount; i++){
+            dots[i] = new ImageView(getContext());
+            dots[i].setImageDrawable(ContextCompat.getDrawable(getContext(), R.drawable.default_dot));
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            params.setMargins(8,0,0,8);
+            sliderDotsPanel.addView(dots[i], params);
+        }
+
+        dots[0].setImageDrawable(ContextCompat.getDrawable(getContext(), R.drawable.selected_dot));
+        pager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                for(int i = 0; i<dotsCount; i++){
+                    dots[i].setImageDrawable(ContextCompat.getDrawable(getContext(), R.drawable.default_dot));
+                }
+                dots[position].setImageDrawable(ContextCompat.getDrawable(getContext(), R.drawable.selected_dot));
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
 
         return rootView;
     }
--- app/src/main/java/com/example/dsm_calendar/MainVPBannerAdapter.java
+++ app/src/main/java/com/example/dsm_calendar/MainVPBannerAdapter.java
@@ -1,7 +1,6 @@
 package com.example.dsm_calendar;
 
 import android.content.Context;
-import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,39 +8,46 @@ import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.viewpager.widget.PagerAdapter;
-
-import java.util.ArrayList;
+import androidx.viewpager.widget.ViewPager;
 
 public class MainVPBannerAdapter extends PagerAdapter {
 
     Context context;
-    ArrayList<Drawable> obj;
+    LayoutInflater inflater;
+    Integer[] obj;
 
-    public MainVPBannerAdapter(Context context, ArrayList<Drawable> obj){
+    public MainVPBannerAdapter(Context context, Integer[] obj){
         this.context = context;
         this.obj = obj;
     }
 
     @Override
     public int getCount() {
-        return obj.size();
+        return obj.length;
     }
 
     @NonNull
     @Override
     public Object instantiateItem(@NonNull ViewGroup container, int position) {
-        View view = null;
-        LayoutInflater inflater = LayoutInflater.from(context);
-        view = inflater.inflate(R.layout.main_vp_banner_adapter, container, false);
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.main_vp_banner_adapter, null);
         ImageView imageView = view.findViewById(R.id.vp_adapter);
-        imageView.setImageDrawable(obj.get(position));
-        container.addView(view);
+        imageView.setImageResource(obj[position]);
 
+        ViewPager vp = (ViewPager) container;
+        vp.addView(view, 0);
         return view;
     }
 
     @Override
     public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
-        return false;
+        return view == object;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        ViewPager vp = (ViewPager) container;
+        View view = (View) object;
+        vp.removeView(view);
     }
 }
