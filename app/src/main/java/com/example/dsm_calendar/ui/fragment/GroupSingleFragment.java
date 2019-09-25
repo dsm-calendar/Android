9c057008f616ccfa4f3a073ab0ccd2c3a6b74004
seung min <3235tmdals@gmail.com>
2019-09-25 21:35:08 +0900

    fab setting, member and animation complete

--- app/src/main/java/com/example/dsm_calendar/ui/fragment/GroupSingleFragment.java
+++ app/src/main/java/com/example/dsm_calendar/ui/fragment/GroupSingleFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,10 +16,14 @@ import androidx.fragment.app.Fragment;
 import com.example.dsm_calendar.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class GroupSingleFragment extends Fragment {
+public class GroupSingleFragment extends Fragment implements View.OnClickListener {
 
-    private FloatingActionButton fab;
+    private FloatingActionButton fab_main;
+    private FloatingActionButton fab_setting;
+    private FloatingActionButton fab_member;
+    private Animation fab_open, fab_hide;
     private NestedScrollView nsv;
+    private Boolean IsFabOpen = false;
 
     public GroupSingleFragment() {}
 
@@ -26,14 +33,22 @@ public class GroupSingleFragment extends Fragment {
         //TODO: add fab to group single fragment
         View rootView = inflater.inflate(R.layout.fragment_group_single, container, false);
 
-        fab = rootView.findViewById(R.id.fab_group_single);
+        fab_main = rootView.findViewById(R.id.fab_group_single_main);
+        fab_setting = rootView.findViewById(R.id.fab_group_single_setting);
+        fab_member = rootView.findViewById(R.id.fab_group_single_member);
         nsv = rootView.findViewById(R.id.nestedScrollView_group_single);
+        fab_open = AnimationUtils.loadAnimation(getActivity(), R.anim.fab_open);
+        fab_hide = AnimationUtils.loadAnimation(getActivity(), R.anim.fab_hide);
+
+        fab_main.setOnClickListener(this);
+        fab_setting.setOnClickListener(this);
+        fab_member.setOnClickListener(this);
 
         nsv.setOnScrollChangeListener((View.OnScrollChangeListener) (v, scrollX, scrollY, oldScrollX, oldScrollY) -> {
             if(scrollY > oldScrollY){
-                fab.hide();
+                fab_main.hide();
             } else {
-                fab.show();
+                fab_main.show();
             }
         });
 
@@ -43,5 +58,36 @@ public class GroupSingleFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.fab_group_single_main:
+                toggleFab();
+                break;
+            case R.id.fab_group_single_setting:
+                Toast.makeText(getActivity(), "setting", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.fab_group_single_member:
+                Toast.makeText(getActivity(), "member", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
+    private void toggleFab(){
+        if(IsFabOpen){
+            fab_setting.startAnimation(fab_hide);
+            fab_member.startAnimation(fab_hide);
+            fab_setting.setClickable(false);
+            fab_member.setClickable(false);
+            IsFabOpen = false;
+        } else {
+            fab_setting.startAnimation(fab_open);
+            fab_member.startAnimation(fab_open);
+            fab_setting.setClickable(true);
+            fab_member.setClickable(true);
+            IsFabOpen = true;
+        }
     }
 }
