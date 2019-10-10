100ca125a1e3a71fdff7374b88ecf243801c52ed
seung min <3235tmdals@gmail.com>
2019-10-10 20:29:28 +0900

    [Add]: GroupActivity findViewById

--- app/src/main/java/com/example/dsm_calendar/ui/activity/GroupActivity.java
+++ app/src/main/java/com/example/dsm_calendar/ui/activity/GroupActivity.java
@@ -1,6 +1,10 @@
 package com.example.dsm_calendar.ui.activity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,9 +13,19 @@ import com.example.dsm_calendar.R;
 
 public class GroupActivity extends AppCompatActivity {
 
+    private TextView groupTitle;
+    private ImageButton showSchedule;
+    private ImageButton showMember;
+    private ImageView groupImage;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group);
+
+        groupTitle = findViewById(R.id.tv_group_name);
+        showSchedule = findViewById(R.id.button_group_schedule);
+        showMember = findViewById(R.id.button_group_member);
+        groupImage = findViewById(R.id.iv_group_image);
     }
 }
