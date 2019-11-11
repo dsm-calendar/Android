59de9d6551fdbac91405dfd4209ce363438c2bea
seung min <3235tmdals@gmail.com>
2019-11-11 22:14:54 +0900

    [Add]: groupSchedule Adapter added(have to make presenter to complete adapter)

--- app/src/main/java/com/example/dsm_calendar/ui/activity/GroupScheduleActivity.java
+++ app/src/main/java/com/example/dsm_calendar/ui/activity/GroupScheduleActivity.java
@@ -1,17 +1,27 @@
 package com.example.dsm_calendar.ui.activity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.dsm_calendar.R;
+import com.example.dsm_calendar.ui.adapter.ScheduleFragmentRVAdapter;
 import com.google.android.material.textfield.TextInputLayout;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 
 public class GroupScheduleActivity extends AppCompatActivity {
 
+    private ImageButton backButton;
+    private MaterialCalendarView calendarView;
     private TextView noListTextView;
+    private RecyclerView recyclerView;
+    private ImageButton addButton;
+    private ScheduleFragmentRVAdapter adapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -19,7 +29,14 @@ public class GroupScheduleActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_group_schedule);
 
+        backButton = findViewById(R.id.button_group_schedule_back);
+        calendarView = findViewById(R.id.cv_group_calendar);
         noListTextView = findViewById(R.id.tv_no_list_group_schedule);
+        recyclerView = findViewById(R.id.rv_group_schedule);
+        addButton = findViewById(R.id.button_group_schedule_add);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
         //TODO: add decorator to calendar
     }
 
---
+++ app/src/main/java/com/example/dsm_calendar/ui/adapter/GroupScheduleRVAdapter.java
@@ -0,0 +1,46 @@
+package com.example.dsm_calendar.ui.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.dsm_calendar.R;
+import com.example.dsm_calendar.data.SampleSchedule;
+import com.example.dsm_calendar.ui.viewHolder.ScheduleViewHolder;
+
+import java.util.ArrayList;
+
+public class GroupScheduleRVAdapter extends RecyclerView.Adapter<ScheduleViewHolder> {
+
+    public ArrayList<SampleSchedule> schedules = new ArrayList<>();
+    private int position;
+
+    @NonNull
+    @Override
+    public ScheduleViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_fragment_schedule, parent, false);
+        ScheduleViewHolder scheduleViewHolder = new ScheduleViewHolder(view, v -> {});
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ScheduleViewHolder holder, int position) {
+        this.position = position;
+        SampleSchedule sampleSchedule = schedules.get(position);
+        holder.bind(sampleSchedule);
+
+        holder.itemView.setOnClickListener( v -> {
+            boolean expanded = sampleSchedule.getExpended();
+            sampleSchedule.setExpended(!expanded);
+            notifyItemChanged(position);
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return schedules.size();
+    }
+}
