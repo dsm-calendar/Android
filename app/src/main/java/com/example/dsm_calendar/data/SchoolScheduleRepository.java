e09c033d927cca210d039ca3eb2376faf8e6dd1b
blAs1N <qazasa123@gmail.com>
2019-11-13 16:16:51 +0900

    Build schedule system

--- app/src/main/java/com/example/dsm_calendar/data/SchoolScheduleRepository.java
+++ app/src/main/java/com/example/dsm_calendar/data/SchoolScheduleRepository.java
@@ -1,7 +1,6 @@
 package com.example.dsm_calendar.data;
 
 import com.example.dsm_calendar.contract.SchoolScheduleContract;
-import com.prolificinteractive.materialcalendarview.CalendarDay;
 
 import java.util.ArrayList;
 
@@ -21,10 +20,8 @@ public class SchoolScheduleRepository implements SchoolScheduleContract.Reposito
     public void getSchedules(GetSchedulesListener listener) {
         ArrayList<SampleSchedule> list = new ArrayList<>();
 
-        list.add((new SampleSchedule("sample title", "2019-11-1", "2019-11-1", "sample content",
-                CalendarDay.from(2019, 10, 1), CalendarDay.from(2019, 10, 1))));
-        list.add(new SampleSchedule("sample title", "2019-11-16", "2019-11-16", "sample content",
-                CalendarDay.from(2019, 10, 16), CalendarDay.from(2019, 10, 16)));
+        list.add(new SampleSchedule("sample title", "2019-11-1", "2019-11-1", "sample content"));
+        list.add(new SampleSchedule("sample title", "2019-11-16", "2019-11-16", "sample content"));
 
         listener.onSuccess(list);
     }
--- app/src/main/java/com/example/dsm_calendar/ui/Decorator/ScheduleDecorator.java
+++ app/src/main/java/com/example/dsm_calendar/ui/Decorator/ScheduleDecorator.java
@@ -1,7 +1,6 @@
 package com.example.dsm_calendar.ui.Decorator;
 
 import android.content.Context;
-import android.text.style.BackgroundColorSpan;
 
 import androidx.core.content.ContextCompat;
 
@@ -10,26 +9,39 @@ import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.DayViewDecorator;
 import com.prolificinteractive.materialcalendarview.DayViewFacade;
 
-import java.util.Collection;
-import java.util.HashSet;
+import java.util.TreeSet;
 
 public class ScheduleDecorator implements DayViewDecorator {
 
-    private HashSet<CalendarDay> list;
+    private TreeSet<Period> list;
     private Context context;
 
-    public ScheduleDecorator(Collection<CalendarDay> list, Context context) {
-        this.list = new HashSet<>(list);
+    public ScheduleDecorator(TreeSet<Period> list, Context context) {
+        this.list = new TreeSet<>(list);
         this.context = context;
     }
 
     @Override
     public boolean shouldDecorate(CalendarDay day) {
-        return list.contains(day);
+        return getContainNum(day) > 0;
     }
 
     @Override
     public void decorate(DayViewFacade view) {
         view.setBackgroundDrawable(ContextCompat.getDrawable(context, R.drawable.view_dot_background));
+    }
+
+    private int getContainNum(CalendarDay day) {
+        int ret = 0;
+
+        for (Period period : list) {
+            if (period.getStart().isAfter(day))
+                break;
+
+            if (period.contain(day))
+                ++ret;
+        }
+
+        return ret;
     }
 }
