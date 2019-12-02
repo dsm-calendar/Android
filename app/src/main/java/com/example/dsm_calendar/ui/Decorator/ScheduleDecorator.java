66a5273e514b9c1b420ee914c5a447179f042768
blAs1N <qazasa123@gmail.com>
2019-12-02 15:46:53 +0900

    Check contain

--- app/src/main/java/com/example/dsm_calendar/ui/Decorator/ScheduleDecorator.java
+++ app/src/main/java/com/example/dsm_calendar/ui/Decorator/ScheduleDecorator.java
@@ -1,32 +1,31 @@
 package com.example.dsm_calendar.ui.Decorator;
 
 import android.content.Context;
+
 import androidx.core.content.ContextCompat;
 
 import com.example.dsm_calendar.R;
-import com.example.dsm_calendar.data.DTO.Schedule;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.DayViewDecorator;
 import com.prolificinteractive.materialcalendarview.DayViewFacade;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
 
 public class ScheduleDecorator implements DayViewDecorator {
 
-    private ArrayList<Schedule> list;
+    private ArrayList<CalendarDay> list;
     private Context context;
 
-    public ScheduleDecorator(Collection<Schedule> list, Context context) {
+    public ScheduleDecorator(Collection<CalendarDay> list, Context context) {
         this.list = new ArrayList<>(list);
         this.context = context;
     }
 
     @Override
     public boolean shouldDecorate(CalendarDay day) {
-        for (Schedule unit : list){
-            if (day.isInRange(unit.getStartDay(), unit.getEndDay()))
+        for (CalendarDay unit : list){
+            if (unit == day)
                 return true;
         }
         return false;
